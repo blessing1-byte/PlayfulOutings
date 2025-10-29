5944080e692e402288c430bc32ac450d492dfea9
Emerald-Jonathan <etieyiboevine@gmail.com>
2025-10-29 12:10:11 +0100

    Done with ContactUs page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import "./App.css";
 import Footer from "./Components/Layout/Footer";
 import Header from "./Components/Layout/Header";
+import ContactUs from "./Components/Pages/ContactUs";
 // import Header from './Components/Layout/Header';
 
 function App() {
   return (
     <>
       <Header />
+      <ContactUs/>
       <Footer />
     </>
   );
